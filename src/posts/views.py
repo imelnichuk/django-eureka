bcf880eedaf22f33863c3eb23fb3c7a3a90512e8
imelnichuk <igor.melnichuk@ukr.net>
2016-12-06 01:37:29 +0200

    Implemented Images uploading/displaying for posts

--- src/posts/views.py
+++ src/posts/views.py
@@ -37,7 +37,7 @@ def post_detail(request, id=None):
     return render(request, 'post_detail.html', context)
 
 def post_create(request):
-    form = PostForm(request.POST or None)
+    form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
@@ -51,7 +51,7 @@ def post_create(request):
 
 def post_update(request, id=None):
     instance = get_object_or_404(Post,id=id)
-    form = PostForm(request.POST or None, instance=instance)
+    form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
